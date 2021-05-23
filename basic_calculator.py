c1eb9d8d3dd064f1b88f357521d6eff2d460941a
Sanskar Singh <sanskar.singh@outlook.com>
2021-05-23 21:46:07 +0530

    fixed multi-operator and mult and div errors

--- basic_calculator.py
+++ basic_calculator.py
@@ -4,32 +4,31 @@ def add(nums):
         sum += nums[i]
     return sum
 
-
 def sub(nums):
     sub = 0
     for i in range(len(nums)):
         sub -= nums[i]
     return sub
 
-
 def mul(nums):
     mul = nums[0]
-    for i in range(len(nums)):
+    for i in range(1, len(nums)): # fixed issue where it would multiply the 1st number twice
         mul *= nums[i]
     return mul
 
-
 def div(nums):
     div = nums[0]
-    for i in range(len(nums)):
+    for i in range(1, len(nums)): # fixed issue where it would divide the 1st number twice
         div /= nums[i]
     return div
 
-
 nums = [int(x) for x in input("Enter the numbers: ").split()]  # can recieve indefinite inputs
-arithmetic = input("Which arithmetic operator to use: ")  # +,-,*,/
+# converted arithmetic to a tuple
+arithmetic = (str(input("Which arithmetic operator to use: "))).split()  # +,-,*,/
 
 arithmetic_operator = {"+": add(nums), "-": sub(nums), "*": mul(nums), "/": div(nums)}
 
-
-print(arithmetic_operator[arithmetic])
+# initialized a for loop for printing the values
+for x in range(len(arithmetic)):
+    str = arithmetic[x]
+    print(arithmetic_operator[str])
